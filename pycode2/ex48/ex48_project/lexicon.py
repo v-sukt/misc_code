39166cd49a3fc8b9e1eb335faa7d0706e7049760
v-sukt <v-sukt@users.noreply.github.com>
2018-08-07 19:22:06 +0530

    Some tests for sentense and parser for lexicon

---
+++ pycode2/ex48/ex48_project/lexicon.py
@@ -0,0 +1,36 @@
+
+game_lexicon = {'direction': ['north','south','east','west','down','up','left','right','back'],
+                'verb' : ['go','stop','kill','eat'],
+                'stop' : ['the','in','of','from','at','it'],
+                'noun': ['door','bear','princess','cabinet']
+                }
+
+def scan(line):
+    '''The function takes the value of line -a string. Splits them into words
+    and scans the game_lexicon appending the matching tuple to the setense and returns a list of tuples'''
+    words = line.split()
+    sentence = []
+    for word in words:
+        found = False
+        for key in game_lexicon.keys():
+        
+            try:        
+                if word.lower() in game_lexicon[key]:
+                    found = True
+                    break
+                elif int(word)%1 == 0:
+                    key, word = 'number', int(word)
+                    found = True
+                    break
+                else:
+                    pass
+                    
+            except:
+                None
+
+        if found:
+            sentence.append((key, word))
+        else:
+            sentence.append(('error',word))
+        
+    return sentence
