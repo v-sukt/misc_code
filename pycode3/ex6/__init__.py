94cddfd42e3d4f15c648d8b800234ab2a9ef4824
v-sukt <v-sukt@users.noreply.github.com>
2019-03-08 14:30:08 +0530

    done ex6 about strings and added new notes

--- pycode3/ex6/__init__.py
+++ pycode3/ex6/__init__.py
@@ -1,1 +1,7 @@
 from .code import *
+
+attempts = 1 \
+           + 1 \
+           + 1
+print("So in total I made", 1+
+      1+1, "attempts")
--- pycode3/ex6/code.py
+++ pycode3/ex6/code.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
-primes = [2]
-for number in range(3,100):
-    is_prime = 0
-    for prime in primes:
-        if number % prime == 0:
-            break
-        else:
-            is_prime = 1
-    if is_prime:
-        primes.append(number)
-    if len(primes) >= 10:
-        print(primes);
-        break
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with right side."
+
+print(w + e)
